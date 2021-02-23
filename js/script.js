c13300a8cbb015af6de7ebded2a7fb4e846b204e
Danil <sedarchukdanil@mail.ru>
2021-02-23 14:03:15 +0600

    Рефакторинг кода , и выполнено первое задание с сайта!

---
+++ js/script.js
@@ -0,0 +1,100 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Скотт Пилигрим против...",
+            "Одержимость",
+            "Афоня",
+            "Лига справедливости",
+            "Ла-ла лэнд"
+        ]
+    };
+    
+     const adv = document.querySelectorAll('.promo__adv img'),
+           poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          ulList =  document.querySelector('.promo__interactive-list'),
+          liList = ulList.querySelectorAll('.promo__interactive-item'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+
+                            //отправитель формы SUBMIT событие
+    addForm.addEventListener('submit', (event) => {
+        // отменяем стандартное поведение браузера, по нажатию кнопки перестает перезагружать страницу
+        event.preventDefault(); 
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0,22)}...`;
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList (movieDB.movies, ulList);
+        }
+
+        event.target.reset();
+
+    });
+    
+
+// удаляет элементы из массива (рекламу с правой стороны сайта)
+const deleteAdv = (arr) => {
+    arr.forEach( item => {
+        item.remove();
+    }); 
+};
+
+
+
+    // меняет текст и картинку 
+    const makeChanges = () => {
+        genre.textContent = "Драма";   
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+
+// сортирует массив
+const sortArr = (arr) => {
+    arr.sort();
+};
+
+ 
+        //Добавляет элементы в список
+    function createMovieList (films, parent) {
+       
+            parent.innerHTML="";
+     
+            films.forEach(function (item ,i) {
+        
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i+1} ${item}
+            <div class="delete"></div>
+            </li>
+            `;
+
+        });
+       
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    sortArr(movieDB.movies);
+    createMovieList (movieDB.movies, ulList);
+
+});
+
+
+
+
+
+  
+
